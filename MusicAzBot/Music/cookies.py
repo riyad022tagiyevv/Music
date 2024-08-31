0e8a2cebd836f68b147e9f7aa1a3ee96e37b56cd
riyad022tagiyevv <97640853+riyad022tagiyevv@users.noreply.github.com>
2024-08-31 21:28:52 +0400

    Update cookies.py

--- MusicAzBot/Music/cookies.py
+++ MusicAzBot/Music/cookies.py
@@ -1,145 +1,25 @@
-#!/usr/bin/python2.7
-# coding=utf-8
-"""
-Ok, Google, your decision about removing WL from the API sucks!
-"""
-import ujson as json
-import os
-from tornado.httpclient import AsyncHTTPClient, HTTPClient, HTTPError, HTTPRequest
-from tornado.ioloop import IOLoop
-import selenium
-from selenium import webdriver
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-import urllib
-import random
+# Netscape HTTP Cookie File
+# This is a generated file! Do not edit.
 
-
-# FIREFOX
-# Will not activate WebDriver plugin unless:
-# xpinstall.signatures.required in about:config
-# https://stackoverflow.com/questions/37247336/selenium-use-of-firefox-profile
-# Doesn't work!
-# Alternative: https://github.com/5digits/dactyl/wiki/Disable-extension-signing-requirement-in-Firefox-49-or-later
-
-class DebugDump(object):
-    def __init__(self, filename):
-        self.fp = open(filename, 'wt')
-
-    def __del__(self):
-        self.fp.close()
-
-    def write(self, b):
-        self.fp.write(b)
-
-FIREFOX_PROFILE_LOCATION = r'%APPDATA%\Mozilla\Firefox\Profiles\<some_profile>'
-FIREFOX_BINARY_LOCATION = r'C:\Program Files (x86)\Mozilla Firefox\firefox.exe'
-
-def downloadWL(filename):
-    videolist = DebugDump(filename)
-    videolist.write('[')
-
-    http = HTTPClient()
-    profile = webdriver.FirefoxProfile(FIREFOX_PROFILE_LOCATION)  # This profile is used as a template, not directly
-    binary = FirefoxBinary(FIREFOX_BINARY_LOCATION)
-    sDriver = webdriver.Firefox(profile, binary, capabilities={"marionette": False})  # https://selenium2.ru/news/188-firefox-esr-52.html
-    sDriver.get('https://www.youtube.com/playlist?list=WL')
-
-    # IMPORTANT! To reuse existing session/cookies, log in using the profile listed as template above! (using your browser, not python)
-    pass
-
-    # Prepare headers for Tornado
-    uagent = sDriver.execute_script("return navigator.userAgent;")
-    cookies = sDriver.get_cookies()
-    cookies = filter(lambda x: 'youtube.com' in x['domain'], cookies)
-    cookies = u'; '.join([u'{}={}'.format(x[u'name'], x[u'value']) for x in cookies])
-    ytcfg = sDriver.execute_script('return ytcfg.data_;')
-    headers = {
-        'X-YouTube-Client-Name': '1',
-        'X-YouTube-Client-Version': str(ytcfg['INNERTUBE_CONTEXT_CLIENT_VERSION']),
-        'X-YouTube-Page-CL': str(ytcfg['PAGE_CL']),
-        'X-Youtube-Identity-Token': ytcfg['ID_TOKEN'],
-        'X-YouTube-Page-Label': ytcfg['PAGE_BUILD_LABEL'],  # responseContext.serviceTrackingParams...key=innertube.build.label,
-        'X-YouTube-Variants-Checksum': str(ytcfg['VARIANTS_CHECKSUM']),
-        'X-YouTube-Utc-Offset': '180',
-        'X-SPF-Referer': 'https://www.youtube.com/playlist?list=WL',
-        'X-SPF-Previous': 'https://www.youtube.com/playlist?list=WL',
-        'Referer': 'https://www.youtube.com/playlist?list=WL',
-        'Cookie': cookies
-    }
-
-    def buildContURL(ct):
-        return u'https://www.youtube.com/browse_ajax?{}'.format(urllib.urlencode({'ctoken': ct['continuation'], 'continuation': ct['continuation'], 'itct': ct['clickTrackingParams']}))
-
-    moreurl = None
-    if 0:
-        # Old design
-        # Get titles already loaded
-        for vid in sDriver.find_elements_by_class_name('pl-video'):
-            print(u'{}: {}'.format(vid.get_attribute('data-title'), vid.get_attribute('data-video-id')))
-
-        # Get link for more
-        morebtn = sDriver.find_element_by_class_name('load-more-button')
-        moreurl = u'https://www.youtube.com' + morebtn.get_attribute('data-uix-load-more-href')
-    else:
-        # New design
-        # JS video list: window["ytInitialData"].contents.twoColumnBrowseResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer.contents[0].itemSectionRenderer.contents[0].playlistVideoListRenderer.contents
-        # JS continuation: window["ytInitialData"].contents.twoColumnBrowseResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer.contents[0].itemSectionRenderer.contents[0].playlistVideoListRenderer.continuations[0].nextContinuationData
-        ct = sDriver.execute_script('return window["ytInitialData"].contents.twoColumnBrowseResultsRenderer.tabs[0].tabRenderer.content.sectionListRenderer.contents[0].itemSectionRenderer.contents[0].playlistVideoListRenderer.continuations[0].nextContinuationData;')
-        #moreurl = u'https://www.youtube.com/browse_ajax?ctoken={}&continuation={}&itct={}'.format(ct['continuation'], ct['continuation'], ct['clickTrackingParams'])
-        moreurl = buildContURL(ct)
-
-    while moreurl is not None:
-        try:
-            resp = http.fetch(HTTPRequest(moreurl, user_agent=uagent, headers=headers))
-            js = json.loads(resp.body)
-            js = js[1]['response']['continuationContents']['playlistVideoListContinuation']
-            for vid in js['contents']:
-                print(u'{}: {}'.format(vid['playlistVideoRenderer']['videoId'], vid['playlistVideoRenderer']['title']['simpleText']))
-            if 'continuations' in js:
-                ct = js['continuations'][0]['nextContinuationData']
-                moreurl = buildContURL(ct)
-                videolist.write(json.dumps(js['contents']))
-            else:
-                moreurl = None
-
-            if moreurl is None:
-                videolist.write(']')
-            else:
-                videolist.write(', ')
-        except HTTPError as e:
-            # HTTPError is raised for non-200 responses; the response
-            # can be found in e.response.
-            print("Error: " + str(e))
-            moreurl = None
-        except Exception as e:
-            # Other errors are possible, such as IOError.
-            print("Error: " + str(e))
-            moreurl = None
-
-    sDriver.close()
-
-
-def loadSavedWL(filename):
-    with open(filename, 'r') as fp:
-        js = json.load(fp)
-
-    videos = []
-    for packet in js:
-        for vid in packet:
-            videos.append((vid['playlistVideoRenderer']['videoId'], vid['playlistVideoRenderer']['title']['simpleText']))
-
-    return videos
-
-
-if __name__ == '__main__':
-    # Load/update the list from Youtube
-	# downloadWL('videolist.txt')
-
-	# Load, shuffle and display
-    videos = loadSavedWL('videolist.txt')
-
-    # TODO: build html page with videos (and thumbnails)
-    random.shuffle(videos)
-    for (vid, title) in videos:
-        print(u'https://www.youtube.com/watch?v={}'.format(vid))
-        print(u'\t{}'.format(title))
+# domain  include_subdomains  path  secure  expiration_date  name  value
+.youtube.com TRUE / TRUE 1755078728 __Secure-1PSIDCC AKEyXzU-AoC4zjeV-oGYaVdnKEZVAEhEg7Q6mDJzJW1X8ZCNknbiKqZw7EG6EBWBObdyHcKn
+.youtube.com TRUE / TRUE 1758102693 __Secure-1PSID g.a000mwgp7u1coDEx6zbDIa4f05M4n1OdWV0fZehtRXzEcoKWdn1zk79pf46JJbz5E21MzCHouwACgYKAXsSARISFQHGX2MiL2eeGJQpLRD50vf06H7flxoVAUF8yKrssFeyaPEN0Y2IpwKd7y6B0076
+.youtube.com TRUE / TRUE 1758102693 __Secure-3PAPISID l8RNSskYvRe-GwIq/Ajg0qeo5oSAKOw4ae
+.youtube.com TRUE / FALSE 1758102693 SID g.a000mwgp7u1coDEx6zbDIa4f05M4n1OdWV0fZehtRXzEcoKWdn1z_b_-xPfoGlh4bhPIZSoPKAACgYKAaQSARISFQHGX2MiNBRtwpntYHUKf_nyHHEdRRoVAUF8yKqfeHo_VSgUx32-1nnP_g0a0076
+.youtube.com TRUE / TRUE 1758102693 __Secure-1PAPISID l8RNSskYvRe-GwIq/Ajg0qeo5oSAKOw4ae
+.youtube.com TRUE / FALSE 1755078728 SIDCC AKEyXzVKxI-t6dqIUEkzw6TVZInBLOzQyPTv5x5iF66nGvRbWCCb7vcEoUhdg_nszYgiJqmq
+.youtube.com TRUE / TRUE 1758102693 __Secure-3PSID g.a000mwgp7u1coDEx6zbDIa4f05M4n1OdWV0fZehtRXzEcoKWdn1ztvT-OUquPsK5EI-asJREbAACgYKAekSARISFQHGX2Mik4R8vp4nDxLxYuSW67tmrxoVAUF8yKpVcajiyKmyjiQnGQwtN5WB0076
+.youtube.com TRUE / TRUE 1755078690 __Secure-1PSIDTS sidts-CjEBUFGohzVdIe1H4yNPYUXQbgd7IlZxgSUKBgDXCyNv3PGEi1e2Kx3PLnzlpW-6lALuEAA
+.youtube.com TRUE / FALSE 1758102693 HSID AsoiTKtkrN204T1xG
+.youtube.com TRUE / TRUE 1758102694 PREF tz=UTC
+.youtube.com TRUE / TRUE 1723542813 YTSESSION-jnufdw ANPz9KgAEeqWLOS76WugWbPjsCGqJzdNfW+Dnm+4TBA3KQs535gs5nvrYdihQr8sMEobCEJOPqePA/7AKN5Yp44T26Qnp2Wf05nOXWw=
+.youtube.com TRUE / TRUE 1758102693 SSID AK3VZ70Ayu7RXRq8w
+.youtube.com TRUE / TRUE 1739094032 VISITOR_PRIVACY_METADATA CgJJThIEGgAgJg%3D%3D
+.youtube.com TRUE / FALSE 1758102693 APISID XRMzfpHz38rFkdYx/AWc3KgKSVfZYBwM_L
+.youtube.com TRUE / TRUE 1755078690 __Secure-3PSIDTS sidts-CjEBUFGohzVdIe1H4yNPYUXQbgd7IlZxgSUKBgDXCyNv3PGEi1e2Kx3PLnzlpW-6lALuEAA
+.youtube.com TRUE / TRUE 1739094032 VISITOR_INFO1_LIVE Dgo57nR5gPk
+.youtube.com TRUE / TRUE 1758102693 SAPISID l8RNSskYvRe-GwIq/Ajg0qeo5oSAKOw4ae
+.youtube.com TRUE / TRUE 1755078728 __Secure-3PSIDCC AKEyXzWVmKx6fQIUks8np30RvCK178qZDGFk55FrV_g32kwajNdMH8FfnMMWlj3zyXcWP1Az
+.youtube.com TRUE / TRUE 1758102693 LOGIN_INFO AFmmF2swRQIgRAS-6tDv0MdSWne-gvmLgJ9mEvPWa4l5gKzlG5crfp4CIQCAmbK0p0bFE5Fv6an6bUWn_DCRgSrXBS4qLYbiM0-yWg:QUQ3MjNmejhPUU9EX1gybjZSaTRTbHRMU0QySnNqMFU1ZkNiZkRPZW96R0N2Um1ndVpBeTVlcnEtZVd3ajdPT0Zjc29wclBrZDNZREU1WXNISDREY2w5SjF3eVNWQzF4S0pPQTJhX0w2bFVYMHJvMGRDV2NfUG1fOWN6VVZ0ZmtuSTZUalBKZUNZOVFFSUhORG5JT21GWDRmbzlPakV6azdR
+.youtube.com TRUE / TRUE 0 YSC K2t0zpHAIfY
+.youtube.com TRUE / TRUE 1548523767 GPS 1
